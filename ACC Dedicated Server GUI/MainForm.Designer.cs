41b7333ab633aaf72c9f2626e7729dd3df495f37
Vindis <martin.vindis.80@gmail.com>
2020-10-07 13:19:21 +0200

    Improved the server startup logic

--- ACC Dedicated Server GUI/MainForm.Designer.cs
+++ ACC Dedicated Server GUI/MainForm.Designer.cs
@@ -2204,7 +2204,7 @@
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ACC Dedicated Server GUI V1.2.6.3 beta";
+            this.Text = "ACC Dedicated Server GUI V1.2.6.3";
             this.Activated += new System.EventHandler(this.MainForm_Activated);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
